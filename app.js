731d2c5350ae173b65e1997d9ee2e36c932f0fc5
Hadi <hadilearn01@gmail.com>
2023-01-31 09:34:33 +0700

    add database integration with mongodb

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-//jshint esversion:6
 
+require('dotenv').config()
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const _ = require('lodash')
+const mongoose = require('mongoose')
 const date = require(__dirname + '/date.js')
 const year = date.getYear()
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const aboutContent = "Hi, I'm Syukri Hadi Kamil. A Developer 'in the making'. I learn Fullstack, but heavily on the Backend. I use JS, Python, and GoLang.";
+const contactContent = "Well, this page still on the early stage of development.";
 
 const app = express();
 
@@ -18,14 +19,38 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
-let blogs = []
+mongoose.set('strictQuery', false)
+// mongoose.connect('mongodb://localhost:27017/blogDB')
+mongoose.connect(`mongodb+srv://${process.env.ATLAS_ID}:${process.env.ATLAS_PW}@cluster0.ouzmlbq.mongodb.net/blogDB`)
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please type the blog title. No title specified.']
+  },
+  content: {
+    type: String,
+    required: [true, 'Please type your blog content. Content empty.']
+  }
+})
+
+const Post = new mongoose.model('Post', postSchema)
 
 app.get('/', (req, res) => {
-  res.render('home', {
-    year: year, pageTitle: 'Daily Journal',
-    // content: homeStartingContent,
-    contents: blogs
+  Post.find({}, (err, foundPosts) => {
+    if (err) {
+      console.log(err)
+    } else {
+      let blogs = foundPosts
+      res.render('home', {
+        year: year,
+        homeStartingContent: homeStartingContent,
+        pageTitle: 'Daily Journal',
+        contents: blogs
+      })
+    }
   })
+  
 })
 
 app.get('/about', (req, res) => {
@@ -41,27 +66,34 @@ app.get('/compose', (req, res) => {
 })
 
 app.post('/compose', (req, res) => {
-  let blog = {}
-  blog.title = req.body.textTitle
-  blog.content = req.body.textPost
-  blogs.push(blog)
+  const blog = new Post ({
+    title: req.body.textTitle,
+    content: req.body.textPost
+  })
+  blog.save()
   res.redirect('/')
 })
 
 app.get('/posts/:postTitle', (req, res) => {
-  // console.log(req.params.postTitle)
   const requestedTitle = _.lowerCase(req.params.postTitle)
-  for (i = 0; i < blogs.length; i++) {
-    const postTitle = _.lowerCase(blogs[i].title)
-    if (requestedTitle === postTitle) {
-      res.render('post', {
-        year: year,
-        pageTitle: blogs[i].title,
-        blog: blogs[i],
-      })
-      break
+  Post.find({}, (err, foundPosts) => {
+    if (err) {
+      console.log(err)
+    } else {
+      let blogs = foundPosts
+      for (i = 0; i < blogs.length; i++) {
+        const postTitle = _.lowerCase(blogs[i].title)
+        if (requestedTitle === postTitle) {
+          res.render('post', {
+            year: year,
+            pageTitle: blogs[i].title,
+            blog: blogs[i],
+          })
+          break
+        }
+      }
     }
-  }
+  })
 })
 
 
